bf1f128a9ed73756912f1ad61f67db4e00503ccd
Michał Księżuk <michal.ksiezuk@hitta.se>
2025-02-05 17:06:29 +0100

    Harpoon and dev icons

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -1,3 +1,8 @@
+local harpoon_status_ok, harpoon = pcall(require, "harpoon")
+if not harpoon_status_ok then
+    return
+end
+
 local mark_status_ok, mark = pcall(require, "harpoon.mark")
 if not mark_status_ok then
     return
@@ -7,6 +12,10 @@ local ui_status_ok, ui = pcall(require, "harpoon.ui")
 if not ui_status_ok then
     return
 end
+
+harpoon.setup({
+    tabline = true
+})
 
 vim.keymap.set("n", "<leader>a", mark.add_file)
 vim.keymap.set("n", "<leader>`", ui.toggle_quick_menu)
